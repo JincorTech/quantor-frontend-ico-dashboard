b148740e3792e47f0a91d586437beb2d771b8144
Alexander <aleksand-isaev@yandex.ru>
2018-06-08 20:03:26 +0400

    Separated verification process

--- src/components/auth/SignUpForm/index.js
+++ src/components/auth/SignUpForm/index.js
@@ -26,7 +26,19 @@ const RestrictedCountriesCodes = [
   'US', 'UM', 'VI', 'CN', 'SG', 'CA', 'KR', 'EC', 'GT', 'VN', 'BD'
 ];
 
+const Step = Object.freeze({
+  Step1: 0,
+  Step2: 1
+});
+
 class SignUpForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      step: Step.Step1
+    };
+  }
+
   componentDidMount() {
     const { change } = this.props;
 
@@ -41,78 +53,65 @@ class SignUpForm extends Component {
     change('source.gtm', gtm);
   }
 
-  render() {
-    const {
-      t,
-      spinner,
-      handleSubmit,
-      invalid,
-      error,
-      referralCode
-    } = this.props;
+  renderStep1 = () => {
+    const { t, spinner, invalid } = this.props;
 
-    const renderReferralField = (code) => {
-      if (code) {
-        this.props.change('referral', code);
-        return (
-          <Field
-            component={RenderInput}
-            name="referral"
-            type="hidden"
-            disabled/>
-        );
-      }
-
-      return (
+    return (
+      <React.Fragment>
         <div className={s.field}>
           <Field
             component={RenderInput}
-            name="referral"
+            name="email"
             type="text"
-            placeholder={t('referralCode')}
-            isBright/>
+            placeholder={t('email')}
+            validate={emailValidate}
+            isBright />
         </div>
-      );
-    };
+
+        <div className={s.field}>
+          <Field
+            component={RenderPassword}
+            name="password"
+            type="password"
+            placeholder={t('password')}
+            validate={passwordValidate} />
+        </div>
+
+        <Button onClick={() => {
+          this.setState({ step: Step.Step2 });
+        }} spinner={spinner} disabled={invalid} isBright>Next step</Button>
+      </React.Fragment>
+    );
+  }
+
+  renderStep2 = () => {
+    const {
+      t, referralCode, spinner, invalid
+    } = this.props;
 
     return (
-      <div>
-        <div className={s.title}>{t('signUp')}</div>
+      <React.Fragment>
+        <div className={s.field}>
+          <Field
+            component={RenderInput}
+            name="firstName"
+            type="text"
+            placeholder={'First Name'}
+            validate={fullNameValidate}
+            isBright />
+        </div>
 
-        {error && <div className={s.error}>{error}</div>}
+        <div className={s.field}>
+          <Field
+            component={RenderInput}
+            name="lastName"
+            type="text"
+            placeholder={'Last Name'}
+            validate={fullNameValidate}
+            isBright />
+        </div>
 
-        <form id="mk_lk_signup" onSubmit={handleSubmit}>
-          <div className={s.field}>
-            <Field
-              component={RenderInput}
-              name="firstName"
-              type="text"
-              placeholder={'First Name'}
-              validate={fullNameValidate}
-              isBright/>
-          </div>
-
-          <div className={s.field}>
-            <Field
-              component={RenderInput}
-              name="lastName"
-              type="text"
-              placeholder={'Last Name'}
-              validate={fullNameValidate}
-              isBright/>
-          </div>
-
-          <div className={s.field}>
-            <Field
-              component={RenderInput}
-              name="email"
-              type="text"
-              placeholder={t('email')}
-              validate={emailValidate}
-              isBright/>
-          </div>
-
-          {/* <div className={s.field}>
+        {/* <div className={s.field}>
             <Field
               component={RenderInput}
               name="phone"
@@ -122,73 +121,111 @@ class SignUpForm extends Component {
               isBright/>
           </div> */}
 
-          {/* <div className={s.phoneHint}>
+        {/* <div className={s.phoneHint}>
             For example +79083971234
           </div> */}
 
-          <Field
-            className={s.select}
-            name="country"
-            component="select"
-            validate={required}>
-            <option value=''>&nbsp;&nbsp;Choose your country...</option>
-            {iso3311a2.getCodes()
-                      .filter((code) => !RestrictedCountriesCodes.includes(code))
-                      .map((code) =>
+        <Field
+          className={s.select}
+          name="country"
+          component="select"
+          validate={required}>
+          <option value=''>&nbsp;&nbsp;Choose your country...</option>
+          {iso3311a2.getCodes()
+            .filter((code) => !RestrictedCountriesCodes.includes(code))
+            .map((code) =>
               <option key={code} value={code}>&nbsp;&nbsp;{iso3311a2.getCountry(code)}</option>)}
-          </Field>
+        </Field>
 
+        <Field
+          className={s.dob}
+          name="dob"
+          component={RenderDatePicker}
+          validate={date} />
+
+        {this.renderReferralField(referralCode)}
+
+        <div className={s.description}>
+          {t('passwordLengthDescription')}
+        </div>
+
+        <div className={s.checkbox}>
           <Field
-            className={s.dob}
-            name="dob"
-            component={RenderDatePicker}
-            validate={date} />
+            component={RenderCheckbox}
+            label={<span>
+              {t('iAgreeWith')} <a href={Globals.agreementLink} target="_blank"><span className={s.terms}>{t('termsOfServices')}</span></a>
+            </span>}
+            name="agreeTos"
+            validate={required} />
+        </div>
 
-          <div className={s.field}>
-            <Field
-              component={RenderPassword}
-              name="password"
-              type="password"
-              placeholder={t('password')}
-              validate={passwordValidate}/>
-          </div>
-
-          {renderReferralField(referralCode)}
-
-          <div className={s.description}>
-            {t('passwordLengthDescription')}
-          </div>
-
-          <div className={s.checkbox}>
-            <Field
-              component={RenderCheckbox}
-              label={<span>
-                {t('iAgreeWith')} <a href={Globals.agreementLink} target="_blank"><span className={s.terms}>{t('termsOfServices')}</span></a>
+        <div className={s.checkbox}>
+          <Field
+            component={RenderCheckbox}
+            label={<span className={s.agreeCountries}>
+              I'm not a citizen or resident of the USA, including District of Columbia,
+              United States Virgins Islands, China, Singapore, Canada,
+              South Korea, Ecuador, Guatemala, Vietnam, Bangladesh.
               </span>}
-              name="agreeTos"
-              validate={required}/>
+            name="agreeCountries"
+            validate={required} />
+        </div>
+
+        <div className={s.agreeCountriesHint}>
+          *Both citizens and residents of these countries
+          are restricted to participate in token-sale
           </div>
 
-          <div className={s.checkbox}>
-            <Field
-              component={RenderCheckbox}
-              label={<span className={s.agreeCountries}>
-                I'm not a citizen or resident of the USA, including District of Columbia,
-                United States Virgins Islands, China, Singapore, Canada,
-                South Korea, Ecuador, Guatemala, Vietnam, Bangladesh.
-              </span>}
-              name="agreeCountries"
-              validate={required}/>
-          </div>
+        <div className={s.button}>
+          <Button type="submit" spinner={spinner} disabled={invalid} isBright>{t('submit')}</Button>
+        </div>
+      </React.Fragment>
+    );
+  }
 
-          <div className={s.agreeCountriesHint}>
-            *Both citizens and residents of these countries
-            are restricted to participate in token-sale
-          </div>
+  renderReferralField = (code) => {
+    const { t } = this.props;
 
-          <div className={s.button}>
-            <Button type="submit" spinner={spinner} disabled={invalid} isBright>{t('submit')}</Button>
-          </div>
+    if (code) {
+      this.props.change('referral', code);
+      return (
+        <Field
+          component={RenderInput}
+          name="referral"
+          type="hidden"
+          disabled />
+      );
+    }
+
+    return (
+      <div className={s.field}>
+        <Field
+          component={RenderInput}
+          name="referral"
+          type="text"
+          placeholder={t('referralCode')}
+          isBright />
+      </div>
+    );
+  };
+
+  render() {
+    const {
+      t,
+      handleSubmit,
+      error
+    } = this.props;
+
+    return (
+      <div>
+        <div className={s.title}>{t('signUp')}</div>
+
+        {error && <div className={s.error}>{error}</div>}
+
+        <form id="mk_lk_signup" onSubmit={handleSubmit}>
+        {
+          this.state.step === Step.Step1 ? this.renderStep1() : this.renderStep2()
+        }
         </form>
 
         <div className={s.footer}>
